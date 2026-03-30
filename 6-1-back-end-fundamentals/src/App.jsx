94bbb9b724e6bbda6b4daa2e6061bb8466eebbb1
Rawan20000 <rawan_rz@icloud.com>
2026-03-30 23:11:15 +0300

    TODO 9

--- 6-1-back-end-fundamentals/src/App.jsx
+++ 6-1-back-end-fundamentals/src/App.jsx
@@ -360,7 +360,8 @@ export default function App() {
   // Create state to store student data
   // Syntax hint:
   // const [student, setStudent] = useState(____);
-  
+   const [student, setStudent] = useState(null);
+
 
   // TODO 8:
   // Request student data from the server when the page loads
